23fcf789cefbcf872dcc01bc8c3d95534888ad77
ocetrn <oce.etourneau@gmail.com>
2021-03-02 18:32:20 +0100

    Création classe Enchere

---
+++ src/fr/eni/enchere/bo/Enchere.java
@@ -0,0 +1,41 @@
+package fr.eni.enchere.bo;
+
+public class Enchere {
+
+	//Attributs
+	private Date dateEnchere;
+	private float montantEnchere;
+	
+	//Constructeurs
+	public Enchere() {
+		//
+	}
+	
+	public Enchere(Date dateEnchere, float montantEnchere) {
+		setDateEnchere(dateEnchere);
+		setMontantEnchere(montantEnchere);
+	}
+
+	//Getter et Setter
+	public Date getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(Date dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public float getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(float montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+	
+	@Override
+	public String toString() {
+		String s = String.format("Date de l'enchère : %s%n Montant de l'enchère : %.2f€%n", getDateEnchere(), getMontantEnchere());
+		return s;
+	}
+}
