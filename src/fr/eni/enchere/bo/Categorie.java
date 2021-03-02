76a7be8454238329e1a37f263d7db84f592c81c2
ocetrn <oce.etourneau@gmail.com>
2021-03-02 18:32:10 +0100

    Création classe Categorie

---
+++ src/fr/eni/enchere/bo/Categorie.java
@@ -0,0 +1,42 @@
+package fr.eni.enchere.bo;
+
+public class Categorie {
+	
+	//Attributs
+	private int noCategorie;
+	private String libelle;
+	
+	//Constructeurs
+	public Categorie() {
+		//
+	}
+
+	public Categorie(int noCategorie, String libelle) {
+		setNoCategorie(noCategorie);
+		setLibelle(libelle);
+	}
+
+	//Getter et Setter
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	@Override
+	public String toString() {
+		String s = String.format("Numéro catégorie : %d%n Libellé : %s%n", getNoCategorie(), getLibelle());
+		return s;
+	}
+	
+}
