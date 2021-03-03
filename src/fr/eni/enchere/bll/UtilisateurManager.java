9a946882606c45ac4c515cc09f6e14381ffa31d0
Ronflexisfunky <Ronflexisfunky@users.noreply.github.com>
2021-03-03 15:30:27 +0100

    creation UtilisateurManager

---
+++ src/fr/eni/enchere/bll/UtilisateurManager.java
@@ -0,0 +1,66 @@
+package fr.eni.enchere.bll;
+
+import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.enchere.dal.jdbc.DAOFactory;
+import fr.eni.enchere.dal.jdbc.UtilisateurDAO;
+
+public class UtilisateurManager {
+	private static UtilisateurDAO daoUsr;
+
+   public UtilisateurManager() throws BLLException {
+        daoUsr = DAOFactory.getUtilisateurDAO();
+    }
+
+    //CRUD: selectById
+    public Utilisateur getUtilisateurById(int id) throws DALException {
+        return daoUsr.selectById(id);
+    }
+    
+    //CRUD: update
+    public void updateUtilisateur (Utilisateur utilisateur, boolean checkForMail, boolean checkForPseudo) throws BLLException, DALException {
+        BLLException bllException = validateUtilisateur(utilisateur);
+        if (checkForMail) {
+            if (!daoUsr.checkForUniquePseudo(utilisateur.getPseudo())) {
+                bllException.addError(BLLErrors.pseudo_taken);
+            }
+        }
+        if (checkForPseudo) {
+            if(!daoUsr.checkForUniqueEmail(utilisateur.getEmail())) {
+                bllException.addError(BLLErrors.email_taken);
+            }
+        }
+
+        if (bllException.hasErrors()) {
+            throw bllException;
+        } else {
+            daoUsr.update(utilisateur);
+        }
+    }
+
+    //CRUD: insert
+    public void insertUtilisateur(Utilisateur utilisateur) throws BLLException, DALException {
+        BLLException bllException = validateUtilisateur(utilisateur);
+        if (!daoUsr.checkForUniquePseudo(utilisateur.getPseudo())) {
+            bllException.addError(BLLErrors.pseudo_taken);
+        }
+        if (!daoUsr.checkForUniqueEmail(utilisateur.getEmail())) {
+            bllException.addError(BLLErrors.email_taken);
+        }
+        if (bllException.hasErrors()) {
+            throw bllException;
+        } else {
+            daoUsr.insert(utilisateur);
+        }
+    }
+    
+    private BLLException validateUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//CRUD: delete
+    public void deleteUtilisateur (Utilisateur utilisateur) throws DALException {
+        daoUsr.delete(utilisateur);
+    }
+
+}
