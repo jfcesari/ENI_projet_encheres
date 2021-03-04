987df1bf9f71c172e3c87008f18a9af4b1404878
Ronflexisfunky <Ronflexisfunky@users.noreply.github.com>
2021-03-04 11:35:16 +0100

    add gestion des erreurs BLL

--- src/fr/eni/enchere/bll/BLLErrors.java
+++ src/fr/eni/enchere/bll/BLLErrors.java
@@ -3,4 +3,12 @@ package fr.eni.enchere.bll;
 public class BLLErrors {
 	static final int pseudo_taken = 30000;
 	static final int email_taken = 30001;
+	static final int length_pseudo = 30002;
+	static final int length_nom = 30003;
+	static final int length_prenom = 30004;
+	static final int length_email = 30005;
+	static final int length_telephone = 30006;
+	static final int length_rue = 30007;
+	static final int length_codepostal = 30008;
+	static final int length_ville = 30009;
 }
