8f8a87b7d45063af5900eaffbd7a668509ddc8d2
jfcesari <jf.cesari@outlook.fr>
2021-03-05 17:29:38 +0100

    fix ?

--- src/fr/eni/enchere/dal/EnchereDAO.java
+++ src/fr/eni/enchere/dal/EnchereDAO.java
@@ -1,5 +1,9 @@
 package fr.eni.enchere.dal;
 
+import java.util.List;
+
+import fr.eni.enchere.bo.Utilisateur;
+
 public class EnchereDAO {
-	List<String>getArticlesByUtilisateur(Utilisateur utilisateur, String articles)
+	List<String>getArticlesByUtilisateur(Utilisateur utilisateur, String articles);
 }
