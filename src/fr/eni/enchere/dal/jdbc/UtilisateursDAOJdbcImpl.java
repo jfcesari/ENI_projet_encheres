e2cb28c87ec8941fa26fcddf3c2aa8d8388b90b7
Ronflexisfunky <Ronflexisfunky@users.noreply.github.com>
2021-03-12 15:53:35 +0100

    rs next in UtilisateursDAOJdbcImpl

--- src/fr/eni/enchere/dal/jdbc/UtilisateursDAOJdbcImpl.java
+++ src/fr/eni/enchere/dal/jdbc/UtilisateursDAOJdbcImpl.java
@@ -35,8 +35,9 @@ private static final String sqlUniqueEmail = "SELECT * FROM utilisateurs WHERE e
 			ps=cnx.prepareStatement(sqlUserSelectbyId);
 			ps.setInt(1, id);
 			rs=ps.executeQuery();
-		
+		while (rs.next()) {
 		utilisateur=new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"),  rs.getString("rue"),  rs.getString("code_postal"),  rs.getString("ville"), rs.getString("mot_de_passe"), rs.getInt("credit"), rs.getBoolean("administrateur"));
+		}
 		cnx.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -59,8 +60,9 @@ private static final String sqlUniqueEmail = "SELECT * FROM utilisateurs WHERE e
 			ps.setString(2, emailouPseudo);
 			ps.setString(3, motDePasse);
 			rs=ps.executeQuery();
-			
+		while (rs.next()) {	
 		utilisateur=new Utilisateur(rs.getInt("no_utilisateur"), rs.getString("pseudo"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("telephone"),  rs.getString("rue"),  rs.getString("code_postal"),  rs.getString("ville"), rs.getString("mot_de_passe"), rs.getInt("credit"), rs.getBoolean("administrateur"));
+		}
 		cnx.close();
         } catch (SQLException e) {
             e.printStackTrace();
